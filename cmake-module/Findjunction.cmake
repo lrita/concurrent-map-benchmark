6b7f83b1c2ec482c8f02d9858e7b5d957f126474
lrita <lrita@163.com>
2020-11-10 17:47:10 +0800

    add turf

--- cmake-module/Findjunction.cmake
+++ cmake-module/Findjunction.cmake
@@ -1,22 +1,19 @@
 include(ExternalProject)
 
 ExternalProject_Add(
-  libtbb
-  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/third-party/tbb
-  SOURCE_DIR ${TOP_SOURCE_DIR}/third-party/tbb
-  CONFIGURE_COMMAND true
-  BUILD_COMMAND
-    $(MAKE) -C ${TOP_SOURCE_DIR}/third-party/tbb extra_inc=big_iron.inc
-    tbb_build_dir=${CMAKE_CURRENT_BINARY_DIR}/third-party/tbb/src/libtbb-build
-    tbb_build_prefix=build
+  libjunction
+  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/third-party/junction
+  SOURCE_DIR ${TOP_SOURCE_DIR}/third-party/junction
+  CMAKE_ARGS -DJUNCTION_WITH_SAMPLES=FALSE
+  BUILD_COMMAND $(MAKE)
   INSTALL_COMMAND true
   BUILD_BYPRODUCTS
     ${CMAKE_CURRENT_BINARY_DIR}/third-party/tbb/src/libtbb-build/build_release/libtbb.a
 )
 
-ExternalProject_Get_Property(libtbb BUILD_BYPRODUCTS)
-ExternalProject_Get_Property(libtbb SOURCE_DIR)
-set(tbb_INCLUDE_DIR "${SOURCE_DIR}/include")
+ExternalProject_Get_Property(libjunction BUILD_BYPRODUCTS)
+ExternalProject_Get_Property(libjunction SOURCE_DIR)
+set(tbb_INCLUDE_DIR "${SOURCE_DIR}")
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(tbb DEFAULT_MSG SOURCE_DIR)
 
