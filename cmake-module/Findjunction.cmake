13446adc7f034a65193f25c5a175317cd6ac2bad
lrita <lrita@163.com>
2020-11-11 12:52:07 +0800

    add junction

--- cmake-module/Findjunction.cmake
+++ cmake-module/Findjunction.cmake
@@ -4,20 +4,38 @@ ExternalProject_Add(
   libjunction
   PREFIX ${CMAKE_CURRENT_BINARY_DIR}/third-party/junction
   SOURCE_DIR ${TOP_SOURCE_DIR}/third-party/junction
-  CMAKE_ARGS -DJUNCTION_WITH_SAMPLES=FALSE
+  CMAKE_ARGS
+    -DJUNCTION_WITH_SAMPLES=FALSE
+    -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/third-party/junction/src/libjunction-build
   BUILD_COMMAND $(MAKE)
-  INSTALL_COMMAND true
+  INSTALL_COMMAND $(MAKE) install
   BUILD_BYPRODUCTS
-    ${CMAKE_CURRENT_BINARY_DIR}/third-party/tbb/src/libtbb-build/build_release/libtbb.a
+    ${CMAKE_CURRENT_BINARY_DIR}/third-party/junction/src/libjunction-build/turf/libturf.a
+    ${CMAKE_CURRENT_BINARY_DIR}/third-party/junction/src/libjunction-build/libjunction.a
 )
 
 ExternalProject_Get_Property(libjunction BUILD_BYPRODUCTS)
 ExternalProject_Get_Property(libjunction SOURCE_DIR)
-set(tbb_INCLUDE_DIR "${SOURCE_DIR}")
+set(junction_INCLUDE_DIR
+    "${CMAKE_CURRENT_BINARY_DIR}/third-party/junction/src/libjunction-build/include"
+)
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(tbb DEFAULT_MSG SOURCE_DIR)
+find_package_handle_standard_args(junction DEFAULT_MSG SOURCE_DIR)
 
-add_library(tbb STATIC IMPORTED)
-set_property(TARGET tbb PROPERTY IMPORTED_LOCATION ${BUILD_BYPRODUCTS})
-set_property(TARGET tbb PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${SOURCE_DIR})
-add_dependencies(tbb libtbb)
+add_library(junction STATIC IMPORTED)
+set_property(
+  TARGET junction
+  PROPERTY
+    IMPORTED_LOCATION
+    ${CMAKE_CURRENT_BINARY_DIR}/third-party/junction/src/libjunction-build/libjunction.a
+)
+add_dependencies(junction libjunction)
+
+add_library(turf STATIC IMPORTED)
+set_property(
+  TARGET turf
+  PROPERTY
+    IMPORTED_LOCATION
+    ${CMAKE_CURRENT_BINARY_DIR}/third-party/junction/src/libjunction-build/turf/libturf.a
+)
+add_dependencies(turf libjunction)
--- main.cpp
+++ main.cpp
@@ -1,30 +1,64 @@
 #include <memory>
 #include <random>
 #include <limits>
+#include <vector>
 #include <assert.h>
 #include <benchmark/benchmark.h>
 #include <tbb/concurrent_hash_map.h>
+#include <junction/ConcurrentMap_Leapfrog.h>
 
-class doc_class { };
+class doc_class {
+  double x = 0;
+};
+struct doc_class_holder {
+  std::shared_ptr<doc_class> doc;
+};
 
 class TestingFixture : public ::benchmark::Fixture {
   const static size_t map_item = 10000000;
 
  public:
   TestingFixture() {
+    id_vec.reserve(map_item);
     for (size_t i = 0; i < map_item; i++) {
-      auto v = std::make_pair(rand_int64(), std::make_shared<doc_class>());
-      tbb_map.insert(std::move(v));
+      auto id  = rand_int64();
+      auto doc = std::make_shared<doc_class>();
+
+      id_vec.push_back(id);
+      tbb_map.insert(std::make_pair(id, doc));
+      junction_map.assign(id, new doc_class_holder {doc});
     }
   };
 
-  int64_t rand_int64() {
+  int64_t rand_int64(int64_t max = std::numeric_limits<int64_t>::max()) {
     thread_local static std::mt19937_64                 rng(std::random_device {}());
     thread_local std::uniform_int_distribution<int64_t> urd;
-    return urd(rng, decltype(urd)::param_type {0, std::numeric_limits<int64_t>::max()});
+    return urd(rng, decltype(urd)::param_type {0, max});
   };
 
+  int64_t rand_id() { return id_vec[rand_int64(id_vec.size())]; }
+
+  std::shared_ptr<doc_class> get_from_tbb_map(int64_t id) {
+    std::shared_ptr<doc_class>        v;
+    decltype(tbb_map)::const_accessor result {};
+    if (tbb_map.find(result, id)) {
+      v = result->second;
+    }
+    return v;
+  }
+
+  std::shared_ptr<doc_class> get_from_junction_map(int64_t id) {
+    std::shared_ptr<doc_class> v;
+    auto                       h = junction_map.get(id);
+    if (h != nullptr) {
+      v = h->doc;
+    }
+    return v;
+  }
+
+  std::vector<int64_t>                                          id_vec;
   tbb::concurrent_hash_map<int64_t, std::shared_ptr<doc_class>> tbb_map;
+  junction::ConcurrentMap_Leapfrog<int64_t, doc_class_holder *> junction_map;
 };
 
 BENCHMARK_DEFINE_F(TestingFixture, get_folly_map)(benchmark::State &st) {
@@ -34,15 +68,17 @@ BENCHMARK_DEFINE_F(TestingFixture, get_folly_map)(benchmark::State &st) {
 
 BENCHMARK_DEFINE_F(TestingFixture, get_tbb_map)(benchmark::State &st) {
   while (st.KeepRunning()) {
-    auto                              id = rand_int64();
-    decltype(tbb_map)::const_accessor result {};
-    tbb_map.find(result, id);
-    benchmark::DoNotOptimize(result);
+    auto id  = rand_id();
+    auto doc = get_from_tbb_map(id);
+    benchmark::DoNotOptimize(doc);
   }
 }
 
 BENCHMARK_DEFINE_F(TestingFixture, get_junction_map)(benchmark::State &st) {
   while (st.KeepRunning()) {
+    auto id  = rand_id();
+    auto doc = get_from_junction_map(id);
+    benchmark::DoNotOptimize(doc);
   }
 }
 
